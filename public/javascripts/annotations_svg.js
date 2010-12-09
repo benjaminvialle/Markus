0a26ac17e06cf1fc7fbd36a3c71282179fa3b08d
Clément Delafargue <divarvel@gmail.com>
2010-12-09 09:51:26 +0100

    Annotations:
    Add: Chops previously drawn paths into 10-point-longs chunks

--- public/javascripts/annotations_svg.js
+++ public/javascripts/annotations_svg.js
@@ -12,13 +12,20 @@ var shape = {
 		}
 	},
 	init: function() {
+		this.shapeCounter = 0;
 		document.addEventListener("mousedown", function(e) {
+			// Disable the drag'n'drop feature for images in
+			// firefox. As the annotated image *is* the background,
+			// this was quite annoying
+			if(e.preventDefault)
+				e.preventDefault();
 			shape.newCurve();
 			document.addEventListener("mousemove", shape.tracker, false);
 		}, false);
 
 		document.addEventListener("mouseup", function(e) {
 			document.removeEventListener("mousemove", shape.tracker, false);
+			shape.finalizeCurve();
 		}, false);
 
 	},
@@ -33,30 +40,67 @@ var shape = {
 		}
 
 		path.setAttribute("d", points);
+		shape.points++;
 	},
 	
 	newCurve: function() {
-		shape.counter++;	  
 		var newGroup = document.createElementNS("http://www.w3.org/2000/svg", "g"),
 			newPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
-		if(shape.counter > 1) {
-			var oldGroup = document.getElementById("shape_current"),
-			    oldPath = oldGroup.firstChild;
-
-			oldGroup.setAttribute("id", "new_shape_" + shape.counter);
-		}
-
 		newGroup.setAttribute("id", "shape_current");
-		newPath.setAttribute("style", "stroke: #FF0000; fill: none;");
 		newPath.setAttribute("d", "");
+		newPath.setAttribute("style", "stroke: #FF0000; fill: none;");
 		newGroup.appendChild(newPath);
 		document.getElementById("shapes").appendChild(newGroup);
 
 	},
+
+	finalizeCurve: function() {
+		console.debug("Finalize");
+		// Moves the old shape
+		var oldGroup = document.getElementById("shape_current"),
+			oldPath = oldGroup.firstChild,
+			points = [];
+
+		points = oldPath.getAttribute("d").split(" ");
+		// Chops the path in 10-node long paths. This is because the
+		// mouseover event is fired when the mouse is over the area
+		// outlined by the path, not the stroke itself.
+		if(points.length > 10) {
+			var currentPath,
+				point;
+			for(var i=0; i<points.length; i++) {
+				// Get the coordinates from the "d" attribute
+				point =  {
+					x: points[i].split(",")[0].substring(1),
+					y: points[i].split(",")[1]
+				};
+				if(currentPath == null || currentPath.getAttribute("d").split(" ").length > 10) {
+					if(currentPath != null) {
+						// The last point is duplicated
+						currentPath.setAttribute("d", currentPath.getAttribute("d")+ " L"+point.x+","+point.y);
+					}
+
+					currentPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+					currentPath.setAttribute("style", "stroke: #FF0000; fill: none;");
+					currentPath.setAttribute("d", "M" + point.x + "," + point.y);
+					oldGroup.appendChild(currentPath);
+				} else {
+					currentPath.setAttribute("d", currentPath.getAttribute("d")+ " L"+point.x+","+point.y);
+				}
+			}
+			oldGroup.removeChild(oldPath);
+
+		}
+
+		oldGroup.setAttribute("id", "new_shape_" + shape.counter);
+		shape.counter++;		
+	},
+
 	counter: 0,
 	lastCoords: null, 
-	lastTime: new Date().getTime()
+	lastTime: new Date().getTime(),
+	points: 0
 	
 };
 document.addEventListener("DOMContentLoaded", shape.init, false)
