3d2801bae0c2998d0df4f8b10d9f9e93395af57d
Jérôme Gazel <jerome@desdemona.(none)>
2010-12-13 14:33:46 +0100

    Modifcation d'AnnotationTextDisplayer pour afficher un fils <g> dans le DOM du SVG pour afficher l'annotation lors du survol

--- public/javascripts/SourceCodeGlower/AnnotationTextDisplayer.js
+++ public/javascripts/SourceCodeGlower/AnnotationTextDisplayer.js
@@ -1,8 +1,8 @@
 /** Annotation Text Displayer Class
 
 This class is in charge of displaying collections of Annotation Texts.  It puts them
-in a DIV with a class called "annotation_text_display" and is in charge of displaying
-that DIV at given coordinates, and hiding that DIV.
+in a G with a class called "annotation_text_display" and is in charge of displaying
+that G at given coordinates, and hiding that G.
 
 Multiple texts are displayed at once, and each one is contained with a <p> tag.
 
@@ -14,14 +14,15 @@ Rules:
 var TEXT_DISPLAY_X_OFFSET = 5;
 var TEXT_DISPLAY_Y_OFFSET = 5;
 
-
 var AnnotationTextDisplayer = Class.create({
+
   initialize: function(parent_node) {
-    //Create the div that we will display in
-    this.display_node = new Element('div', {'class': 'annotation_text_display', 'onmousemove': 'hide_image_annotations()'});
+    //Create the G that we will display in
+    this.display_node = new Element('g', {'class': 'annotation_text_display', 'onmousemove': 'hide_image_annotations()'});
     $(parent_node).appendChild(this.display_node);
     this.hide();
   },
+  
   //Assumes collection is subclass of Prototype Enumerable class
   //x and y is the location on the screen where this collection will display
   displayCollection: function(collection, x, y) {
@@ -30,47 +31,60 @@ var AnnotationTextDisplayer = Class.create({
     //Return if the collection is empty
     if(collection.length == 0){ return;}
     //Now, compile all the annotations in this collection into a single
-    //string to display.  Each text will be contained in a <p> tag
+    //string to display.  Each text will be contained in a new paragraph
     var final_string = '';
     collection.each(function(annotation_text) {
-      final_string += "<p>" + annotation_text.getContent() + "BISOUH"+ "</p>";
+      final_string += annotation_text.getContent() + "/n/n";
     });
     
-    //Update the Display node (a div, in this case) to be in the right
+    //Update the Display node (a g, in this case) to be in the right
     //position, and to have the right contents
-    final_string = final_string.replace(/\n/g, '<br/>');
+    //final_string = final_string.replace(/\n/g, '<br/>');
     this.updateDisplayNode(final_string, x, y);
     
     //Show the Displayer
     this.show();
   },
+  
+  
   //Hide all showing annotations.
   hideShowing: function() {
     if(this.getShowing()) {
       this.hide();
     }
   },
+  
+  
   updateDisplayNode: function(text, x, y) {
     var display_node = $(this.getDisplayNode());
     display_node.update(text);
-    display_node.setStyle({
-      left: (x + TEXT_DISPLAY_X_OFFSET) + 'px',
-      top: (y + TEXT_DISPLAY_Y_OFFSET) + 'px'
-    });
+    //display_node.setStyle({
+    //  left: (x + TEXT_DISPLAY_X_OFFSET) + 'px',
+    //  top: (y + TEXT_DISPLAY_Y_OFFSET) + 'px'
+    //});
+    display_node.setAttribute("transform","translate(" + (x + TEXT_DISPLAY_X_OFFSET) + "," + (y + TEXT_DISPLAY_Y_OFFSET) + ")");
   },
+  
+  
   //Hide the displayer
   hide: function() {
-    $(this.display_node).hide();
+    console.debug(this.display_node);
+    this.display_node.setAttribute('style','display:none');
   },
+  
+  
   //Show the displayer
   show: function() {
-    $(this.display_node).show();
+    this.display_node.setAttribute('style','display:block');
   },
+
   //Returns whether or not the Displayer is showing
   getShowing: function() {
     return this.getDisplayNode().visible;
   },
-  //Returns the DIV that we're displaying in
+  
+  
+  //Returns the G that we're displaying in
   getDisplayNode: function() {
     return $(this.display_node);
   }
