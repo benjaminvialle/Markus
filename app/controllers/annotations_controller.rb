f393d4e0963af2c0169bfa43ca0991ea1bb9f3ab
Benjamin Vialle <benjaminvialle@gmail.com>
2011-05-01 10:32:13 +0200

    Fix: Typo

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -107,8 +107,8 @@ class AnnotationsController < ApplicationController
       @size = ImageSize.new(open(File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path, @submission_file.filename)).read).size if @submission_file.is_supported_image?
       @size = ImageSize.new(open(File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path, @submission_file.filename.split('.').first + '.jpg')).read).size if @submission_file.is_pdf?
     rescue Errno::ENOENT
-      raise "File #{@submission_file.filename} in #{File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path)} not found - Are you using SQLite3 as database" if @submission_file.is_pdf?
-      raise "File #{@submission_file.filename.split('.').first}.jpg in #{File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path)} not found - Are you using SQLite3 as database" if @submission_file.is_supported_image?
+      raise "File #{@submission_file.filename} in #{File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path)} not found - Are you using SQLite3 as database?" if @submission_file.is_pdf?
+      raise "File #{@submission_file.filename.split('.').first}.jpg in #{File.join(MarkusConfigurator.markus_config_pdf_storage, @submission_file.submission.grouping.group.repository_name, @submission_file.path)} not found - Are you using SQLite3 as database?" if @submission_file.is_supported_image?
     end
     if grouping.ensure_can_see?(current_user)
       @annotations = @submission_file.annotations
