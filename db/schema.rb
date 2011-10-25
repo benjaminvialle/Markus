6066b57677802d3c4e4d61c5cb750c422553f298
Benjamin Vialle <benjaminvialle@gmail.com>
2011-10-25 09:30:40 +0200

    Updated Gemfile and schema.rb to be more consistent

--- db/schema.rb
+++ db/schema.rb
@@ -255,13 +255,6 @@ ActiveRecord::Schema.define(:version => 20110313200240) do
 
   add_index "periods", ["submission_rule_id"], :name => "index_periods_on_submission_rule_id"
 
-  create_table "point", :force => true do |t|
-    t.integer "shape_id"
-    t.integer "order"
-    t.integer "x"
-    t.integer "y"
-  end
-
   create_table "points", :force => true do |t|
     t.integer "shape_annotation_id"
     t.integer "order"
@@ -322,10 +315,6 @@ ActiveRecord::Schema.define(:version => 20110313200240) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
-  create_table "shape", :force => true do |t|
-    t.integer "annotation_id"
-  end
 
   create_table "submission_collectors", :force => true do |t|
     t.integer "child_pid"
